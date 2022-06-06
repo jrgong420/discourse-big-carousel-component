fba26093be44ac53504430d2cd31f8644d0cf3cc
Kris <kris.aubuchon@discourse.org>
2022-06-06 09:21:01 -0400

    Add ability to show user in carousel w/ posts, improve functionality  (#3)

--- javascripts/discourse/components/big-carousel.js
+++ javascripts/discourse/components/big-carousel.js
@@ -3,31 +3,75 @@ import { computed } from "@ember/object";
 import loadScript from "discourse/lib/load-script";
 import discourseComputed from "discourse-common/utils/decorators";
 import { inject as service } from "@ember/service";
-import { iconNode } from "discourse-common/lib/icon-library";
+import { ajax } from "discourse/lib/ajax";
+import { set } from "@ember/object";
+import { Promise } from "rsvp";
 
 export default Component.extend({
   router: service(),
   bigSlides: computed(function () {
     return JSON.parse(settings.big_carousel_slides);
   }),
+  isLoading: true,
 
   ensureSlider() {
-    if (this.shouldDisplay && this.bigSlides.length > 1) {
-      return loadScript(settings.theme_uploads.tiny_slider).then(() => {
-        var slider = tns({
-          container: ".custom-big-carousel-slides",
-          items: 1,
-          controls: true,
-          autoplay: settings.big_carousel_autoplay,
-          speed: settings.big_carousel_speed,
-          prevButton: ".custom-big-carousel-prev",
-          nextButton: ".custom-big-carousel-next",
-          navContainer: ".custom-big-carousel-nav",
-        });
+    let bigStaticSlides = [];
 
-        console.log(slider.getInfo()); // temp
+    if (this.shouldDisplay && this.bigSlides.length > 1) {
+      // set up static populated slides
+      this.bigSlides.forEach((slide) => {
+        if (slide.slide_type === "slide") {
+          bigStaticSlides.push(slide);
+        }
       });
+
+      let bigUserSlides = [];
+
+      // get user info
+      const userSlides = this.bigSlides.filterBy("slide_type", "user");
+      let userSetup = Promise.all(
+        userSlides.map((slide) => {
+          return ajax(`/u/${slide.link}.json`).then((result) => {
+            set(slide, "user_info", result);
+          });
+        })
+      );
+
+      // get user activity
+      let userActivity = Promise.all(
+        userSlides.map((slide) => {
+          return ajax(
+            `user_actions.json?offset=0&username=${slide.link}&filter=5`
+          ).then((result) => {
+            set(slide, "user_activity", result.user_actions.slice(0, 3));
+            bigUserSlides.push(slide);
+          });
+        })
+      );
+
+      Promise.all([userSetup, userActivity])
+        .then(() => {
+          this.set("bigUserSlides", bigUserSlides);
+          loadScript(settings.theme_uploads.tiny_slider).then(() => {
+            // slider script
+            var slider = tns({
+              container: ".custom-big-carousel-slides",
+              items: 1,
+              controls: true,
+              autoplay: settings.big_carousel_autoplay,
+              speed: settings.big_carousel_speed,
+              prevButton: ".custom-big-carousel-prev",
+              nextButton: ".custom-big-carousel-next",
+              navContainer: ".custom-big-carousel-nav",
+            });
+          });
+        })
+        .finally(() => {
+          this.set("isLoading", false);
+        });
     }
+
+    this.set("bigStaticSlides", bigStaticSlides);
   },
 
   @discourseComputed("router.currentRouteName")
